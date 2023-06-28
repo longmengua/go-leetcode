5d1c6188e48f5c4a9761f4ff50b44239aafc1e10
Waltor <longmengua@gmail.com>
2023-06-28 19:14:26 +0800

    add Q1 answer

---
+++ leetcode/Q1.ans.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func TwoSumAns(nums []int, target int) []int {
+	indexMap := make(map[int]int)
+	for currIndex, currNum := range nums {
+		if requiredIdx, isPresent := indexMap[target-currNum]; isPresent {
+			return []int{requiredIdx, currIndex}
+		}
+		indexMap[currNum] = currIndex
+	}
+	return []int{}
+}
