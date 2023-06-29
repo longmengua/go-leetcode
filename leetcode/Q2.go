ae7ad7055d6c86f8b33b521d68155aff875d0802
Waltor <longmengua@gmail.com>
2023-06-29 09:38:01 +0800

    Q2

--- leetcode/Q2.go
+++ leetcode/Q2.go
@@ -9,18 +9,16 @@ type ListNode struct {
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	var toReturn *ListNode
 	var ans *ListNode
-	ln1 := l1
-	ln2 := l2
 	carry := 0
 
-	for ln1 != nil || ln2 != nil {
-		newV := ln1.Val + ln2.Val + carry
+	for l1 != nil || l2 != nil || carry > 0 {
+		newV := l1.Val + l2.Val + carry
 		carry = newV / 10
 		newL := &ListNode{
 			Val: newV % 10,
 		}
-		ln1 = ln1.Next
-		ln2 = ln2.Next
+		l1 = l1.Next
+		l2 = l2.Next
 		if ans == nil {
 			ans = newL
 			toReturn = newL
