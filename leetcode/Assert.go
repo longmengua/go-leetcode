45b571dcea03621132a91c6e3271e4e835d4fa09
Waltor <longmengua@gmail.com>
2023-06-29 12:57:29 +0800

    Q3

--- leetcode/Assert.go
+++ leetcode/Assert.go
@@ -4,6 +4,6 @@ import "testing"
 
 func Assert(t *testing.T, assert any, result any) {
 	if assert != result {
-		t.Errorf("Error, %s, %s", result, assert)
+		t.Errorf("Error, %s, %s", assert, result)
 	}
 }
--- leetcode/Q3.go
+++ leetcode/Q3.go
@@ -5,23 +5,19 @@ compare to longest length number when hit the duplicated digit everytime
 */
 func LengthOfLongestSubstring(s string) int {
 	longest := 0
-	currLen := 0
 	head := 0
 	m := make(map[int32]int)
 
 	// rune type is int32, which means unicode.
 	for index, v := range s {
 		if _, exist := m[v]; exist {
-			if m[v] > head {
-				head = m[v]
+			if m[v] >= head {
+				head = m[v] + 1
 			}
-			currLen = index - head
-		} else {
-			currLen++
 		}
 		m[v] = index
-		if currLen > longest {
-			longest = currLen
+		if index-head+1 > longest {
+			longest = index - head + 1
 		}
 	}
 	return longest
